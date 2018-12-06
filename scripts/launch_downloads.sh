0b57c7f5fa1c1016c42d2911838955cea056dff2
Matt Elbé <matt.leboucher@gmail.com>
2018-12-06 21:38:58 +0100

    Fixes array.

--- scripts/launch_downloads.sh
+++ scripts/launch_downloads.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/scripts"
-MODEL_NAMES=("ResNet50", "ResNet101", "ResNet152", "MobileNetV2", "InceptionV4", "SEResNeXt50", "SEResNeXt101")
+MODEL_NAMES=("ResNet50" "ResNet101" "ResNet152" "MobileNetV2" "InceptionV4" "SEResNeXt50" "SEResNeXt101")
 
 for model_name in "${MODEL_NAMES[@]}"; do
     sbatch "$SCRIPT_PATH"/download_weights.sh ${model_name}
