d6f4bc62e1e6a0ffa5539eae1d6f563e1f5a5790
Matt Elbé <matt.leboucher@gmail.com>
2018-12-07 16:55:10 +0100

    Adds FC-DenseNet56.

--- scripts/launch_trainings.sh
+++ scripts/launch_trainings.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 SCRIPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/scripts"
-MODEL_NAMES=("DeepLabV3_plus" "PSPNet" "BiSeNet")
+MODEL_NAMES=("FC-DenseNet56" "DeepLabV3_plus" "PSPNet" "BiSeNet")
 
 for model_name in "${MODEL_NAMES[@]}"; do
     sbatch "$SCRIPT_PATH"/train.sh ${model_name}
