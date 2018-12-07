43c5cab3d30728d6db8f5ead05e5b75a5876f948
Matt Elbé <matt.leboucher@gmail.com>
2018-12-07 17:03:07 +0100

    Updates column width.

--- train.py
+++ train.py
@@ -170,8 +170,9 @@ if not os.path.exists(results_path):
     os.makedirs(results_path)
 
 headers = ['epoch', 'avg_accuracy', 'precision', 'recall', 'f1', 'miou']
-header_format = '{:15}' * len(headers)
-row_format = '{:15.12f}' * len(headers)
+
+header_format = '{:13}' * len(headers)
+row_format = '{:2.4f}' * len(headers)
 with open(os.path.join(results_path, "results.txt"), "a+") as results_file:
     results_file.write(header_format.format(*headers))
 
