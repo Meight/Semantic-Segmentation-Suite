b7d53767ca07365b65ba9027401a563bea5bf718
Matt Elbé <matt.leboucher@gmail.com>
2018-12-11 13:32:21 +0100

    Fixes scripts.

--- scripts/launch_predictions.sh
+++ scripts/launch_predictions.sh
@@ -3,6 +3,6 @@
 SCRIPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/scripts"
 
 IMAGE="/projets/thesepizenberg/deep-learning/segmentation-suite/voc-ha/val"
-CKPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/checkpoints/BiSeNet/ResNet101/non-augmented/0058"
+CKPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/checkpoints/BiSeNet/ResNet101/non-augmented/0058/model.ckpt"
 
 sbatch "$SCRIPT_PATH"/predict.sh ${IMAGE} ${CKPT_PATH} BiSeNet voc-ha
