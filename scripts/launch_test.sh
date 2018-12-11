9ce9a27feefc3ac7f2de6810a569017b8a70aac5
Matt Elbé <matt.leboucher@gmail.com>
2018-12-11 14:43:35 +0100

    Fixes script path.

--- scripts/launch_test.sh
+++ scripts/launch_test.sh
@@ -4,4 +4,4 @@ SCRIPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/scripts"
 
 CKPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/checkpoints/BiSeNet/ResNet101/non-augmented/0058/model.ckpt"
 
-sbatch "$SCRIPT_PATH"/predict.sh ${CKPT_PATH} BiSeNet voc-ha
+sbatch "$SCRIPT_PATH"/test.sh ${CKPT_PATH} BiSeNet voc-ha
