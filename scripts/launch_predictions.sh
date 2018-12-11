e9e39d8f4a8cfea1386dc825920ae9c03504b5b7
Matt Elbé <matt.leboucher@gmail.com>
2018-12-11 14:20:07 +0100

    Fixes image path.

--- scripts/launch_predictions.sh
+++ scripts/launch_predictions.sh
@@ -2,7 +2,7 @@
 
 SCRIPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/scripts"
 
-IMAGE="/projets/thesepizenberg/deep-learning/segmentation-suite/voc-ha/val"
+IMAGE="/projets/thesepizenberg/deep-learning/segmentation-suite/voc-ha/val/2007_000033.jpg"
 CKPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/checkpoints/BiSeNet/ResNet101/non-augmented/0058/model.ckpt"
 
 sbatch "$SCRIPT_PATH"/predict.sh ${IMAGE} ${CKPT_PATH} BiSeNet voc-ha
