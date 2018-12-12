257e8a45ab8938014cb6bcee96fd3fc76cc4ef10
Matt Elbé <matt.leboucher@gmail.com>
2018-12-12 17:05:53 +0100

    Changes order of execution.

--- scripts/launch_trainings.sh
+++ scripts/launch_trainings.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 SCRIPT_PATH="/projets/thesepizenberg/deep-learning/segmentation-suite/scripts"
-MODEL_NAMES=("BiSeNet FC-DenseNet103 GCN DeepLabV3_plus RefineNet DenseASPP PSPNet")
+MODEL_NAMES=("BiSeNet FC-DenseNet103 GCN DeepLabV3_plus RefineNet DenseASPP PSPNet DDSC AdapNet")
 FRONTEND_NAMES=("ResNet101")
 INPUT_SIZE=(256 384 512)
 BATCH_SIZE=(1)
 DATASET="voc-chh"
 
-for model_name in "${MODEL_NAMES[@]}"; do
-    for frontend_name in "${FRONTEND_NAMES[@]}"; do
-        for input_size in "${INPUT_SIZE[@]}"; do
-            for batch_size in "${BATCH_SIZE[@]}"; do
+for input_size in "${INPUT_SIZE[@]}"; do
+    for batch_size in "${BATCH_SIZE[@]}"; do
+        for model_name in "${MODEL_NAMES[@]}"; do
+            for frontend_name in "${FRONTEND_NAMES[@]}"; do
                 sbatch "$SCRIPT_PATH"/train.sh ${model_name} ${frontend_name} ${input_size} ${batch_size} ${DATASET}
             done;
         done;
