f53c4a7590a5b4410a97a13294df01853823fcf2
Matt Elbé <matt.leboucher@gmail.com>
2018-12-12 17:36:53 +0100

    Prints normalized histogram.

--- scripts/launch_analysis.sh
+++ scripts/launch_analysis.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=/projets/thesepizenberg/deep-learning/logs/analysis.out
 #SBATCH --error=/projets/thesepizenberg/deep-learning/logs/analysis.out
 
-#SBATCH --ntasks=1
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=24CPUNodes
 #SBATCH --mem-per-cpu=7500M
@@ -12,7 +12,7 @@
 set -e
 
 # Various script and dataset paths.
-DATASET_NAMES=("voc-chh")
+DATASET_NAMES=("voc-chh" "voc-ha")
 GROUND_TRUTH_MASKS_DIR="/projets/thesepizenberg/deep-learning/deeplab-generic/matlab/Masques"
 ANALYSIS_SCRIPT="/projets/thesepizenberg/deep-learning/segmentation-suite"
 
@@ -22,7 +22,7 @@ for dataset_name in "${DATASET_NAMES[@]}"; do
     echo "Launching analysis for dataset ${dataset_name}."
     srun -n1 -N1 /projets/thesepizenberg/deep-learning/deeplab-generic/matlab/venv/bin/python3.4 \
             "${ANALYSIS_SCRIPT}/analysis.py" --ground-truth-masks-directory=${GROUND_TRUTH_MASKS_DIR} \
-            --dataset-name=${dataset_name}
+            --dataset-name=${dataset_name} &
 done;
 
 wait
