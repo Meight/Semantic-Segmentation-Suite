ca6957fc7db173eb47e4e959e9c561251f2e946e
Matt Elbé <matt.leboucher@gmail.com>
2018-12-12 17:40:24 +0100

    Prints normalized histogram.

--- scripts/launch_analysis.sh
+++ scripts/launch_analysis.sh
@@ -4,7 +4,7 @@
 #SBATCH --output=/projets/thesepizenberg/deep-learning/logs/analysis.out
 #SBATCH --error=/projets/thesepizenberg/deep-learning/logs/analysis.out
 
-#SBATCH --ntasks=1
+#SBATCH --ntasks=2
 #SBATCH --cpus-per-task=8
 #SBATCH --partition=24CPUNodes
 #SBATCH --mem-per-cpu=7500M
