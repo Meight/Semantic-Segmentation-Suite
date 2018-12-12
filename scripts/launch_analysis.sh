b9a5746400ba5b8778b194154fba828a682f36b3
Matt Elbé <matt.leboucher@gmail.com>
2018-12-12 16:26:22 +0100

    Adds Open CV.

--- scripts/launch_analysis.sh
+++ scripts/launch_analysis.sh
@@ -16,7 +16,7 @@ DATASET_NAMES=("chh")
 GROUND_TRUTH_MASKS_DIR="/projets/thesepizenberg/deep-learning/deeplab-generic/matlab/Masques"
 ANALYSIS_SCRIPT="/projets/thesepizenberg/deep-learning/segmentation-suite"
 
-srun /projets/thesepizenberg/deep-learning/deeplab-generic/matlab/venv/bin/pip3 install opencv-python
+srun /projets/thesepizenberg/deep-learning/deeplab-generic/matlab/venv/bin/pip3 install sklearn2
 
 wait
 
